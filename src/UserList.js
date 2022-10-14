b9e7155321c9821c749e43739a78d326b70b895a
lyeskandsi@gmail.com <lyeskandsi>
2022-10-14 10:41:49 +0100

    two

--- src/UserList.js
+++ src/UserList.js
@@ -1,30 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function UserList() {
-    let [data,setData]=useState();
 
- const result=()=>{fetch('https://jsonplaceholder.typicode.com/users')
-  .then((Response)=>Response.json())
-  .then((json)=>
-  {console.log(json);
-    setData(json);
-    
-})}
-useEffect(()=>{result()},[])
-    return (
-    
-         <div>
-         <button onClick={result}>result</button>
+  let [data, setData] = useState([]);
 
-         <div><ul>{data.map((item)=><li>{item.name}</li>)}</ul></div>
 
-</div>
+    function result(){
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
 
-         
-        
-    
-          
-    )
+    useEffect(() => {
+        result();
+      }, []);
+
+      
+  return (
+    <div>
+      <button onClick={result}>result</button>
+
+      <div>
+        <ul>
+          {data.map((item,key) => (
+            <li key={key}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default UserList;
